8012f04d61b2c2bf5cdec7334d8c55cb2e2858ec
yohanesrickyyy <yohanesrickyyy@gmail.com>
2024-03-14 14:53:49 +0700

    fix(handler): Update UI CLI folder Show Product

--- handler/show_product.go
+++ handler/show_product.go
@@ -1,32 +1,42 @@
 package handler
 
 import (
-	"PairProjectPhase1/config"
-	"fmt"
+    "PairProjectPhase1/config"
+    "fmt"
 )
 
 func ShowProduct() {
-	query := `SELECT p.product_name, p.description, s.size_name, pd.price, pd.stock FROM products p JOIN products_detail pd ON p.product_id = pd.product_id JOIN sizes s ON pd.size_id = s.size_id;`
-	rows, err := config.DB.Query(query)
-	if err != nil {
-		panic(err.Error())
-	}
+    query := `
+        SELECT p.product_name, p.description, s.size_name, pd.price, pd.stock
+        FROM products p 
+        JOIN products_detail pd ON p.product_id = pd.product_id 
+        JOIN sizes s ON pd.size_id = s.size_id;
+    `
+    rows, err := config.DB.Query(query)
+    if err != nil {
+        panic(err.Error())
+    }
 
-	for rows.Next() {
-		var name, description, size_name string
-		var stock int
-		var price float64
-		err = rows.Scan(&name, &description, &size_name, &price, &stock)
-		if err != nil {
-			panic(err.Error())
-		}
+    // Header tabel
+    fmt.Printf("| %-5s | %-20s | %-70s | %-10s | %-14s | %-5s |\n", "No", "Name", "Description", "Size Name", "Price", "Stock")
+    fmt.Println("|-------|----------------------|------------------------------------------------------------------------|------------|----------------|-------|")
 
-		fmt.Println("Name: ", name)
-		fmt.Println("Description: ", description)
-		fmt.Println("Size Name: ", size_name)
-		fmt.Println("Price: ", price)
-		fmt.Println("Stock: ", stock)
-		fmt.Println("---------------------------------------------")
+    // Variabel untuk nomor urut
+    var count int
 
-	}
+    for rows.Next() {
+        var name, description, sizeName string
+        var stock int
+        var price float64
+        err = rows.Scan(&name, &description, &sizeName, &price, &stock)
+        if err != nil {
+            panic(err.Error())
+        }
+
+        // Increment nomor urut
+        count++
+
+        // Cetak baris tabel
+        fmt.Printf("| %-5d | %-20s | %-70s | %-10s | Rp.%-11.2f | %-5d |\n", count, name, description, sizeName, price, stock)
+    }
 }
